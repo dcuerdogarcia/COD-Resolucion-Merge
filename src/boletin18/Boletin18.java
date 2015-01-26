fdb46ea924ea48d0d3628431d7b7dae99c585df2
dcuerdogarcia <dcuerdogarcia@eniac10.danielcastelao.org>
2015-01-26 09:04:42 +0100

    Commit inicial

---
+++ src/boletin18/Boletin18.java
@@ -0,0 +1,20 @@
+package boletin18;
+
+public class Boletin18 {
+
+    public static void main(String[] args) {
+        int[] numeros = new int[6];
+        for (int i = 0; i < 6; i++) {
+            double random = Math.random() * 50 + 1;
+            int valor = (int) random;
+            numeros[i] = valor;
+            System.out.println("Elemento " + (i + 1) + " --> " + numeros[i]);
+        }
+
+        System.out.println("\n");
+
+        for (int i = numeros.length - 1; i >= 0; i--) {
+            System.out.println("Elemento " + (i + 1) + " --> " + numeros[i]);
+        }
+    }
+}
